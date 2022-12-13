a108289eb3adbd112898f74ec0efc6e6497d84c3
Ricky Hu <lakeseafly@icloud.com>
2022-12-13 09:28:22 +0800

    Update 1.run_hifiasm.sh

--- 1.Assembly/1.run_hifiasm.sh
+++ 1.Assembly/1.run_hifiasm.sh
@@ -1,4 +1,4 @@
 ### Using the haplotype-resolved de novo assembler for PacBio HiFi reads.
 
-hifiasm -o Clipper  -t 16 Clipper.Q20.fasta -l 0 2> run_asm.Clipper.log
-hifiasm -o Stirling  -t 16 Stirling.Q20.fasta -l 0 2> run_asm.Stirling.log
+hifiasm -o Clipper  -t 16 Clipper.Q20.fasta 2> run_asm.Clipper.log
+hifiasm -o Stirling  -t 16 Stirling.Q20.fasta 2> run_asm.Stirling.log
